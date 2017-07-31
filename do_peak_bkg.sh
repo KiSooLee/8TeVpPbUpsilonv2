f7f2a07e212e7f5a66bf323bdd9b311da57b098c
KiSooLee <kilee@cern.ch>
2017-07-31 23:21:58 +0900

    old correlate codes are removed

--- do_peak_bkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-root -l -b -q 'Correl_peak_bkg.C+(false, 0, 300, 0, 30, 0, 1, "v1")'
-root -l -b -q 'Correl_peak_bkg.C+(false, 0, 300, 0, 30, 1, 3, "v1")'
-root -l -b -q 'Correl_peak_bkg.C+(false, 0, 300, 0, 30, 3, 10, "v1")'
