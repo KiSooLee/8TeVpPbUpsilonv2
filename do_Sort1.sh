6c6ff82a132fd5942a60c4f57bfb3060beee50b9
Kisoo Lee <kilee@cern.ch>
2018-03-05 16:04:51 +0900

    old file removed

--- do_Sort1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Sort1.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
-
-version="v1"
-root -l -b -q 'Sort1.C+(false, 0, 300, 0, 30, -2.4, 2.4, 0, 1, '\"$version\"')'
-root -l -b -q 'Sort1.C+(false, 0, 300, 0, 30, -2.4, 2.4, 1, 3, '\"$version\"')'
-root -l -b -q 'Sort1.C+(false, 0, 300, 0, 30, -2.4, 2.4, 3, 6, '\"$version\"')'
--- do_Sort7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Sort7.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
-
-version="v1"
-root -l -b -q 'Sort7.C+(false, 0, 300, 0, 30, -2.4, 2.4, 0, 1, '\"$version\"')'
-root -l -b -q 'Sort7.C+(false, 0, 300, 0, 30, -2.4, 2.4, 1, 3, '\"$version\"')'
-root -l -b -q 'Sort7.C+(false, 0, 300, 0, 30, -2.4, 2.4, 3, 6, '\"$version\"')'
