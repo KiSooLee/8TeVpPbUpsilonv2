73f580473a561ee4c54d8e5b2aef608f6eee2673
Kisoo Lee <kilee@cern.ch>
2018-06-02 15:59:56 +0900

    unused argument removed

--- do_mSortpPb1.sh
+++ do_mSortpPb1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Sort0.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
 
-#source /cvmfs/cms.cern.ch/cmsset_default.sh
-#scram p CMSSW CMSSW_8_0_26_patch2
-#cd CMSSW_8_0_26_patch2
-#eval `scramv1 runtime -sh`
-#cd -
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scram p CMSSW CMSSW_8_0_26_patch2
+cd CMSSW_8_0_26_patch2
+eval `scramv1 runtime -sh`
+cd -
 
-version="v1"
-#root -l -b -q 'mSortpPb1.C+(false, 0, 1500, 0, 30, -2.4, 2.4, 0, 10, '\"$version\"', '$1')'
-#root -l -b -q 'Collect_pPb1.C+(false, 120, 1500, 0, 6, -2.4, 2.4, 1, 3, '$1')'
-root -l -b -q 'Collect_pPb1.C+(false, 120, 1500, 0, 6, -2.4, 2.4, 1, 3, 117)'
+#root -l -b -q 'mSortpPb1.C+(false, 0, 1500, 0, 30, -2.4, 2.4, 0, 10, '$1')'
+root -l -b -q 'Collect_pPb1.C+(false, 110, 300, 10, 30, -2.4, 2.4, 1, 3, '$1')'
+#root -l -b -q 'Collect_pPb1.C+(false, 110, 1500, 10, 30, -2.4, 2.4, 1, 3, 0)'
