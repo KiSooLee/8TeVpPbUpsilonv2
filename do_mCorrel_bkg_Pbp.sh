ae40f3f76f1f8de2ff9be581e1f6ea80d79cfbe7
Kisoo Lee <kilee@cern.ch>
2018-06-02 16:40:04 +0900

    version notation is adjusted

--- do_mCorrel_bkg_Pbp.sh
+++ do_mCorrel_bkg_Pbp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Sort0.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
 
-version="v11"
+#version="v11"
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram p CMSSW CMSSW_8_0_26_patch2
@@ -9,6 +9,6 @@ cd CMSSW_8_0_26_patch2
 eval `scramv1 runtime -sh`
 cd -
 
-root -l -b -q 'mCorrel_bkg_Pbp.C+(false, 120, 1500, 0, 4, -2.4, 2.4, 1, 3, '\"$version\"', '$1', 20)'
+root -l -b -q 'mCorrel_bkg_Pbp.C+(false, 110, 300, 0, 4, -2.4, 2.4, 1, 3, "v13", '$1')'
 #ls
-#root -l -b -q 'mCorrel_bkg_Pbp.C+(false, 120, 1500, 0, 6, -2.4, 2.4, 1, 3, '\"$version\"', 16, 20)'
+#root -l -b -q 'mCorrel_bkg_Pbp.C+(false, 120, 1500, 10, 30, -2.4, 2.4, 1, 3, "v13", 5)'
