7ae2802c121a0b5d7b927de3774e112484620097
Kisoo Lee <kilee@cern.ch>
2018-07-30 15:49:13 +0900

    header name and save files added

--- SkimmedFiles/Yield/YieldDist.C
+++ SkimmedFiles/Yield/YieldDist.C
@@ -10,7 +10,7 @@
 #include <TLegend.h>
 #include <TMath.h>
 #include "../../Upsilon.h"
-#include "../../Style_Kisoo.h"
+#include "../../Style_Upv2.h"
 //}}}
 
 void YieldDist(TString version = "v1")
@@ -22,30 +22,46 @@ void YieldDist(TString version = "v1")
 	TCanvas* c2S = new TCanvas("c2S", "", 0, 0, 600, 600);
 	TCanvas* c3S = new TCanvas("c3S", "", 0, 0, 600, 600);
 	TCanvas* call = new TCanvas("call", "", 0, 0, 600, 600);
-	TH1D* h1S;
-	TH1D* h2S;
-	TH1D* h3S;
+	TCanvas* c1SN = new TCanvas("c1SN", "", 0, 0, 600, 600);
+	TCanvas* c2SN = new TCanvas("c2SN", "", 0, 0, 600, 600);
+	TCanvas* c3SN = new TCanvas("c3SN", "", 0, 0, 600, 600);
+	TCanvas* callN = new TCanvas("callN", "", 0, 0, 600, 600);
+	TH1D* h1S, * h1SN;
+	TH1D* h2S, * h2SN;
+	TH1D* h3S, * h3SN;
 	if(mult_narr > 2)
 	{
 		h1S = new TH1D("h1S", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 		h2S = new TH1D("h2S", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 		h3S = new TH1D("h3S", ";multiplicity;Counts", mult_narr-1, multBinsArr);
+		h1SN = new TH1D("h1SN", ";multiplicity;Counts", mult_narr-1, multBinsArr);
+		h2SN = new TH1D("h2SN", ";multiplicity;Counts", mult_narr-1, multBinsArr);
+		h3SN = new TH1D("h3SN", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 	}
 	else if(pt_narr > 2)
 	{
 		h1S = new TH1D("h1S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h2S = new TH1D("h2S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h3S = new TH1D("h3S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
+		h1SN = new TH1D("h1SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
+		h2SN = new TH1D("h2SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
+		h3SN = new TH1D("h3SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 	}
 	else if(rap_narr > 2)
 	{
 		h1S = new TH1D("h1S", ";y;Counts", rap_narr-1, rapBinsArr);
 		h2S = new TH1D("h2S", ";y;Counts", rap_narr-1, rapBinsArr);
 		h3S = new TH1D("h3S", ";y;Counts", rap_narr-1, rapBinsArr);
+		h1SN = new TH1D("h1SN", ";y;Counts", rap_narr-1, rapBinsArr);
+		h2SN = new TH1D("h2SN", ";y;Counts", rap_narr-1, rapBinsArr);
+		h3SN = new TH1D("h3SN", ";y;Counts", rap_narr-1, rapBinsArr);
 	}
 	FormTH1Marker(h1S, 0, 1, 1);
 	FormTH1Marker(h2S, 1, 1, 1);
 	FormTH1Marker(h3S, 2, 1, 1);
+	FormTH1Marker(h1SN, 0, 1, 1);
+	FormTH1Marker(h2SN, 1, 1, 1);
+	FormTH1Marker(h3SN, 2, 1, 1);
 //}}}
 
 	for(Int_t imult = 0; imult < mult_narr-1; imult++)
@@ -67,36 +83,54 @@ void YieldDist(TString version = "v1")
 					w1S = h1S->GetBinWidth(imult+1);
 					w2S = h2S->GetBinWidth(imult+1);
 					w3S = h3S->GetBinWidth(imult+1);
-					h1S->SetBinContent(imult+1, (double)hin->GetBinContent(1)/w1S);
-					h1S->SetBinError(imult+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
-					h2S->SetBinContent(imult+1, (double)hin->GetBinContent(2)/w2S);
-					h2S->SetBinError(imult+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
-					h3S->SetBinContent(imult+1, (double)hin->GetBinContent(3)/w3S);
-					h3S->SetBinError(imult+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
+					h1S->SetBinContent(imult+1, (double)hin->GetBinContent(1));
+					h1S->SetBinError(imult+1, hin->GetBinError(1));
+					h2S->SetBinContent(imult+1, (double)hin->GetBinContent(2));
+					h2S->SetBinError(imult+1, hin->GetBinError(2));
+					h3S->SetBinContent(imult+1, (double)hin->GetBinContent(3));
+					h3S->SetBinError(imult+1, hin->GetBinError(3));
+					h1SN->SetBinContent(imult+1, (double)hin->GetBinContent(1)/w1S);
+					h1SN->SetBinError(imult+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
+					h2SN->SetBinContent(imult+1, (double)hin->GetBinContent(2)/w2S);
+					h2SN->SetBinError(imult+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
+					h3SN->SetBinContent(imult+1, (double)hin->GetBinContent(3)/w3S);
+					h3SN->SetBinError(imult+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
 				}
 				else if (pt_narr > 2)
 				{
 					w1S = h1S->GetBinWidth(ipt+1);
 					w2S = h2S->GetBinWidth(ipt+1);
 					w3S = h3S->GetBinWidth(ipt+1);
-					h1S->SetBinContent(ipt+1, (double)hin->GetBinContent(1)/w1S);
-					h1S->SetBinError(ipt+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
-					h2S->SetBinContent(ipt+1, (double)hin->GetBinContent(2)/w2S);
-					h2S->SetBinError(ipt+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
-					h3S->SetBinContent(ipt+1, (double)hin->GetBinContent(3)/w3S);
-					h3S->SetBinError(ipt+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
+					h1S->SetBinContent(ipt+1, (double)hin->GetBinContent(1));
+					h1S->SetBinError(ipt+1, hin->GetBinError(1));
+					h2S->SetBinContent(ipt+1, (double)hin->GetBinContent(2));
+					h2S->SetBinError(ipt+1, hin->GetBinError(2));
+					h3S->SetBinContent(ipt+1, (double)hin->GetBinContent(3));
+					h3S->SetBinError(ipt+1, hin->GetBinError(3));
+					h1SN->SetBinContent(ipt+1, (double)hin->GetBinContent(1)/w1S);
+					h1SN->SetBinError(ipt+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
+					h2SN->SetBinContent(ipt+1, (double)hin->GetBinContent(2)/w2S);
+					h2SN->SetBinError(ipt+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
+					h3SN->SetBinContent(ipt+1, (double)hin->GetBinContent(3)/w3S);
+					h3SN->SetBinError(ipt+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
 				}
 				else if(rap_narr > 2)
 				{
 					w1S = h1S->GetBinWidth(irap+1);
 					w2S = h2S->GetBinWidth(irap+1);
 					w3S = h3S->GetBinWidth(irap+1);
-					h1S->SetBinContent(irap+1, (double)hin->GetBinContent(1)/w1S);
-					h1S->SetBinError(irap+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
-					h2S->SetBinContent(irap+1, (double)hin->GetBinContent(2)/w2S);
-					h2S->SetBinError(irap+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
-					h3S->SetBinContent(irap+1, (double)hin->GetBinContent(3)/w3S);
-					h3S->SetBinError(irap+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
+					h1S->SetBinContent(irap+1, (double)hin->GetBinContent(1));
+					h1S->SetBinError(irap+1, hin->GetBinError(1));
+					h2S->SetBinContent(irap+1, (double)hin->GetBinContent(2));
+					h2S->SetBinError(irap+1, hin->GetBinError(2));
+					h3S->SetBinContent(irap+1, (double)hin->GetBinContent(3));
+					h3S->SetBinError(irap+1, hin->GetBinError(3));
+					h1SN->SetBinContent(irap+1, (double)hin->GetBinContent(1)/w1S);
+					h1SN->SetBinError(irap+1, hin->GetBinError(1)/TMath::Sqrt(w1S));
+					h2SN->SetBinContent(irap+1, (double)hin->GetBinContent(2)/w2S);
+					h2SN->SetBinError(irap+1, hin->GetBinError(2)/TMath::Sqrt(w2S));
+					h3SN->SetBinContent(irap+1, (double)hin->GetBinContent(3)/w3S);
+					h3SN->SetBinError(irap+1, hin->GetBinError(3)/TMath::Sqrt(w3S));
 				}
 //}}}
 
@@ -107,15 +141,48 @@ void YieldDist(TString version = "v1")
 	h1S->SetMinimum(0);
 	h2S->SetMinimum(0);
 	h3S->SetMinimum(0);
+	h1SN->SetMinimum(0);
+	h2SN->SetMinimum(0);
+	h3SN->SetMinimum(0);
+
 	c1S->cd();
-	h1S->Draw();
+	h1S->Draw("petext0");
+	TLatex* lt1 = new TLatex();
+	lt1->SetNDC();
+	lt1->SetTextSize(0.04);
+	lt1->DrawLatex(0.54,0.70,"p_{T}^{#mu} > 4.0 GeV/c");
+	lt1->DrawLatex(0.54,0.63,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
+	lt1->DrawLatex(0.54,0.56,Form("%d < mult. < %d GeV/c", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
+	lt1->DrawLatex(0.54,0.49,"|y^{#Upsilon}| < 2.4");
 	c1S->SaveAs(Form("Yield_distribution_1S_%s.pdf", version.Data()));
+
+	c1SN->cd();
+	h1SN->Draw();
+	TLatex* lt1N = new TLatex();
+	lt1N->SetNDC();
+	lt1N->SetTextSize(0.04);
+	lt1N->DrawLatex(0.54,0.70,"p_{T}^{#mu} > 4.0 GeV/c");
+	lt1N->DrawLatex(0.54,0.63,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
+	lt1N->DrawLatex(0.54,0.56,Form("%d < mult. < %d GeV/c", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
+	lt1N->DrawLatex(0.54,0.49,"|y^{#Upsilon}| < 2.4");
+	c1SN->SaveAs(Form("Yield_distribution_1SN_%s.pdf", version.Data()));
+
 	c2S->cd();
-	h2S->Draw();
+	h2S->Draw("petext0");
 	c2S->SaveAs(Form("Yield_distribution_2S_%s.pdf", version.Data()));
+
+	c2SN->cd();
+	h2SN->Draw();
+	c2SN->SaveAs(Form("Yield_distribution_2SN_%s.pdf", version.Data()));
+
 	c3S->cd();
-	h3S->Draw();
+	h3S->Draw("petext0");
 	c3S->SaveAs(Form("Yield_distribution_3S_%s.pdf", version.Data()));
+
+	c3SN->cd();
+	h3SN->Draw();
+	c3SN->SaveAs(Form("Yield_distribution_3SN_%s.pdf", version.Data()));
+
 	call->cd();
 	h1S->Draw();
 	h2S->Draw("same");
@@ -127,4 +194,16 @@ void YieldDist(TString version = "v1")
 	l1->AddEntry(h3S, "3S", "pl");
 	l1->Draw();
 	call->SaveAs(Form("Yield_distribution_all_%s.pdf", version.Data()));
+
+	callN->cd();
+	h1SN->Draw();
+	h2SN->Draw("same");
+	h3SN->Draw("same");
+	TLegend* l1N = new TLegend(0.7, 0.7, 0.9, 0.9);
+	FormLegend(l1N, 0.04);
+	l1N->AddEntry(h1S, "1S", "pl");
+	l1N->AddEntry(h2S, "2S", "pl");
+	l1N->AddEntry(h3S, "3S", "pl");
+	l1N->Draw();
+	callN->SaveAs(Form("Yield_distribution_allN_%s.pdf", version.Data()));
 }
