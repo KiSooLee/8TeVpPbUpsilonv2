8a5f57814c3a3f5eb58b2fdf206092425c056ef1
Kisoo Lee <kilee@cern.ch>
2018-09-01 18:38:52 +0900

    code for upsilon acceptance check

---
+++ do_Cand2D.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+root -l -b -q 'Candidate2D.C+(110, 300, 0, 4, -2.4, 2.4, 0, 3, "v16")'
+root -l -b -q 'Candidate2D.C+(110, 300, 4, 7, -2.4, 2.4, 0, 3, "v16")'
+root -l -b -q 'Candidate2D.C+(110, 300, 7, 10, -2.4, 2.4, 0, 3, "v16")'
+root -l -b -q 'Candidate2D.C+(110, 300, 10, 30, -2.4, 2.4, 0, 3, "v16")'
